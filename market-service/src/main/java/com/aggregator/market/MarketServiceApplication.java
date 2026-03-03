a895671ce05b5a4f44590659d985f98b7af29530
wojteksuj <wojciechsuj@gmail.com>
2026-03-03 20:47:05 +0100

    Rename package

--- market-service/src/main/java/com/aggregator/market/MarketServiceApplication.java
+++ market-service/src/main/java/com/aggregator/market/MarketServiceApplication.java
@@ -1,4 +1,4 @@
-package org.example.marketservice;
+package com.aggregator.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- market-service/src/test/java/com/aggregator/market/MarketServiceApplicationTests.java
+++ market-service/src/test/java/com/aggregator/market/MarketServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.marketservice;
+package com.aggregator.market;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
