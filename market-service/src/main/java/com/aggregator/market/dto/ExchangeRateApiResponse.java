ed0eac72e3dafba483b45161f4974e8eb3d82dab
wojteksuj <wojciechsuj@gmail.com>
2026-03-23 21:51:27 +0100

    Add dto to fetch response from external api

---
+++ market-service/src/main/java/com/aggregator/market/dto/ExchangeRateApiResponse.java
@@ -0,0 +1,13 @@
+package com.aggregator.market.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public record ExchangeRateApiResponse(
+        @JsonProperty("result") String result,
+        @JsonProperty("base_code") String baseCode,
+        @JsonProperty("conversion_rates") Map<String, BigDecimal> conversionRates
+)
+{}
