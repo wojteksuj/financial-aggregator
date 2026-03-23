d9e186943918eb1f4f36353d0f934a96b2d622e5
wojteksuj <wojciechsuj@gmail.com>
2026-03-23 22:07:41 +0100

    Add method for fetching rates

--- market-service/src/main/java/com/aggregator/market/service/ExchangeRateClient.java
+++ market-service/src/main/java/com/aggregator/market/service/ExchangeRateClient.java
@@ -1,15 +1,30 @@
 package com.aggregator.market.service;
 
 import com.aggregator.market.config.ExchangeRateApiConfig;
-import org.springframework.stereotype.Service;
+import com.aggregator.market.dto.ExchangeRateApiResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
-@Service
+@Component
 public class ExchangeRateClient {
     private final ExchangeRateApiConfig exchangeRateApiConfig;
 
     public ExchangeRateClient(ExchangeRateApiConfig exchangeRateApiConfig) {
         this.exchangeRateApiConfig = exchangeRateApiConfig;
     }
+
+    public ExchangeRateApiResponse fetchRates(){
+        RestClient restClient = RestClient.create();
+
+        ExchangeRateApiResponse response = restClient.get()
+                .uri(exchangeRateApiConfig.getBaseUrl() + "/" + exchangeRateApiConfig.getApiKey() + "/latest/" + exchangeRateApiConfig.getBaseCurrency())
+                .retrieve()
+                .body(ExchangeRateApiResponse.class);
+        return response;
+    }
+
+
+
 
 
 }
