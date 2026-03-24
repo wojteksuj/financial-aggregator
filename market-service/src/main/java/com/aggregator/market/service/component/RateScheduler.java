e850a92436a3ab5cbda89324fc24e65d272dc339
wojteksuj <wojciechsuj@gmail.com>
2026-03-24 21:15:03 +0100

    Add scheduler for fetching rates every 1 minute

---
+++ market-service/src/main/java/com/aggregator/market/service/component/RateScheduler.java
@@ -0,0 +1,19 @@
+package com.aggregator.market.service.component;
+
+import com.aggregator.market.service.RateService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RateScheduler {
+    private final RateService rateService;
+
+    public RateScheduler(RateService rateService) {
+        this.rateService = rateService;
+    }
+
+    @Scheduled(fixedDelay = 6000)
+    public void scheduleFetch(){
+        rateService.fetchAndSave();
+    }
+}
