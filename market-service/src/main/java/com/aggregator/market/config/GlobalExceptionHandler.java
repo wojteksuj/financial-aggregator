9e3c94a1bfb5c1ca4c71fc14ec670f25c3345e2e
wojteksuj <wojciechsuj@gmail.com>
2026-03-24 21:52:58 +0100

    Add custom handle for MethodArgumentTypeMismatchException

--- market-service/src/main/java/com/aggregator/market/config/GlobalExceptionHandler.java
+++ market-service/src/main/java/com/aggregator/market/config/GlobalExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.aggregator.market.config;
 
 import com.aggregator.market.exception.ExchangeRateApiException;
-import org.springframework.beans.factory.parsing.Problem;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,5 +13,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ExchangeRateApiException.class)
     public ProblemDetail handleExchangeRateApiException(ExchangeRateApiException ex){
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_GATEWAY, ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ProblemDetail handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex){
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 }
