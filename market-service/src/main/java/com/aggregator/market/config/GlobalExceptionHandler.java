6b83549fff3c73b847e0ebca063667ed00ce882b
wojteksuj <wojciechsuj@gmail.com>
2026-03-24 21:36:48 +0100

    Add handler for ExchangeRateApiException

--- market-service/src/main/java/com/aggregator/market/config/GlobalExceptionHandler.java
+++ market-service/src/main/java/com/aggregator/market/config/GlobalExceptionHandler.java
@@ -1,8 +1,17 @@
 package com.aggregator.market.config;
 
+import com.aggregator.market.exception.ExchangeRateApiException;
+import org.springframework.beans.factory.parsing.Problem;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(ExchangeRateApiException.class)
+    public ProblemDetail handleExchangeRateApiException(ExchangeRateApiException ex){
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_GATEWAY, ex.getMessage());
+    }
 }
