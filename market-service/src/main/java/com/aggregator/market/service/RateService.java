55acee1b54a04d8980823280c3297e9661dd4a77
wojteksuj <wojciechsuj@gmail.com>
2026-03-24 21:07:25 +0100

    Add fetchAndSave method

--- market-service/src/main/java/com/aggregator/market/service/RateService.java
+++ market-service/src/main/java/com/aggregator/market/service/RateService.java
@@ -1,20 +1,25 @@
 package com.aggregator.market.service;
 
 import com.aggregator.market.dto.RateResponseDto;
+import com.aggregator.market.entity.ExchangeRate;
 import com.aggregator.market.repository.ExchangeRateRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class RateService {
     private final ExchangeRateRepository rateRepository;
+    private final ExchangeRateClient exchangeRateClient;
 
-    public RateService(ExchangeRateRepository rateRepository) {
+    public RateService(ExchangeRateRepository rateRepository, ExchangeRateClient exchangeRateClient) {
         this.rateRepository = rateRepository;
+        this.exchangeRateClient = exchangeRateClient;
     }
 
     public List<RateResponseDto> getRates(String currencyCode, LocalDate date) {
@@ -25,5 +30,22 @@ public class RateService {
                 .stream()
                 .map(rate -> new RateResponseDto(rate.getCurrencyCode(), rate.getBaseCurrency(), rate.getFetchedAt(), rate.getRate()))
                 .toList();
+    }
+
+    public void fetchAndSave() {
+        Set<String> wantedCurrencies = Set.of("EUR", "USD", "GBP", "CHF", "CZK", "JPY");
+        var rates = exchangeRateClient.fetchRates();
+
+        List<ExchangeRate> savedRates = rates.conversionRates().entrySet().stream()
+                .filter(entry -> wantedCurrencies.contains(entry.getKey()))
+                .map(entry -> new ExchangeRate(
+                        entry.getKey(),
+                        rates.baseCode(),
+                        entry.getValue(),
+                        Instant.now()
+                ))
+                .toList();
+
+        rateRepository.saveAll(savedRates);
     }
 }
