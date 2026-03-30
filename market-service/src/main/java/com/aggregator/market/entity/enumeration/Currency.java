c7a758d41b78c8010edf29f4a6c31f8925514b95
wojteksuj <wojciechsuj@gmail.com>
2026-03-30 14:38:13 +0200

    Add static method to get list of codes of all currencies

--- market-service/src/main/java/com/aggregator/market/entity/enumeration/Currency.java
+++ market-service/src/main/java/com/aggregator/market/entity/enumeration/Currency.java
@@ -1,5 +1,9 @@
 package com.aggregator.market.entity.enumeration;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 public enum Currency {
     EURO("EUR"),
     UNITED_STATES_DOLLAR("USD"),
@@ -16,5 +20,9 @@ public enum Currency {
 
     public String getCurrencyCode() {
         return currencyCode;
+    }
+
+    public static List<String> getAllCodes(){
+        return Arrays.stream(values()).map(Currency::getCurrencyCode).toList();
     }
 }
