06a8c05bfc589754d223c19111a730a58dc6cad8
wojteksuj <wojciechsuj@gmail.com>
2026-03-30 16:40:51 +0200

    Add Mockito extenstion

--- market-service/src/test/java/com/aggregator/market/RateServiceTest.java
+++ market-service/src/test/java/com/aggregator/market/RateServiceTest.java
@@ -1,4 +1,9 @@
 package com.aggregator.market;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class RateServiceTest {
+
 }
