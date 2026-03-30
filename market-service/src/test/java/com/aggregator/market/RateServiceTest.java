0041b6fd3dc119c91da6b06c2f8469f6b859589f
wojteksuj <wojciechsuj@gmail.com>
2026-03-30 18:10:26 +0200

    Add unit test for invalid date in getRates

--- market-service/src/test/java/com/aggregator/market/RateServiceTest.java
+++ market-service/src/test/java/com/aggregator/market/RateServiceTest.java
@@ -1,12 +1,18 @@
 package com.aggregator.market;
 
+import com.aggregator.market.exception.InvalidRequestException;
 import com.aggregator.market.repository.ExchangeRateRepository;
 import com.aggregator.market.service.RateService;
 import com.aggregator.market.service.component.ExchangeRateClient;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
 
 @ExtendWith(MockitoExtension.class)
 public class RateServiceTest {
@@ -16,4 +22,11 @@ public class RateServiceTest {
     private ExchangeRateClient exchangeRateClient;
     @InjectMocks
     private RateService rateService;
+
+    @Test
+    void testGetRatesInvalidDate(){
+        String currencyCode = "USD";
+        LocalDate date = LocalDate.now().plusDays(1);
+        assertThrows(InvalidRequestException.class, () -> rateService.getRates(currencyCode, date));
+    }
 }
