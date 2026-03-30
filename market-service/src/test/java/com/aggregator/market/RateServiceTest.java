4a95ddb7b675551dcf3b389031972b19f8b59bd8
wojteksuj <wojciechsuj@gmail.com>
2026-03-30 16:42:15 +0200

    Set up and inject mocks

--- market-service/src/test/java/com/aggregator/market/RateServiceTest.java
+++ market-service/src/test/java/com/aggregator/market/RateServiceTest.java
@@ -1,9 +1,19 @@
 package com.aggregator.market;
 
+import com.aggregator.market.repository.ExchangeRateRepository;
+import com.aggregator.market.service.RateService;
+import com.aggregator.market.service.component.ExchangeRateClient;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class RateServiceTest {
-
+    @Mock
+    private ExchangeRateRepository exchangeRateRepository;
+    @Mock
+    private ExchangeRateClient exchangeRateClient;
+    @InjectMocks
+    private RateService rateService;
 }
