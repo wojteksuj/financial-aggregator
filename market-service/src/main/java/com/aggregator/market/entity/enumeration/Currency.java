0220c26f79bc9eaea0d37a9903c3182c8b6dd54a
wojteksuj <wojciechsuj@gmail.com>
2026-03-30 14:32:46 +0200

    Add field currencyCode and change values names

--- market-service/src/main/java/com/aggregator/market/entity/enumeration/Currency.java
+++ market-service/src/main/java/com/aggregator/market/entity/enumeration/Currency.java
@@ -1,10 +1,20 @@
 package com.aggregator.market.entity.enumeration;
 
 public enum Currency {
-    EUR,
-    USD,
-    GBP,
-    CHF,
-    CZK,
-    JPY
+    EURO("EUR"),
+    UNITED_STATES_DOLLAR("USD"),
+    POUND_STERLING("GBP"),
+    SWISS_FRANC("CHF"),
+    CZECH_CROWN("CZK"),
+    JAPANESE_YEN("JPY");
+
+    private final String currencyCode;
+
+    Currency(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
 }
