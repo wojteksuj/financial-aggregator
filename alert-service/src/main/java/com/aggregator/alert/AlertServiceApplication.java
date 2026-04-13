b8dffdc7e8e20b0cafe3e37c2c2319c19a379843
wsujczynski <wojciechsuj@gmail.com>
2026-04-13 17:58:07 +0200

    Rename paths

--- alert-service/src/main/java/com/aggregator/alert/AlertServiceApplication.java
+++ alert-service/src/main/java/com/aggregator/alert/AlertServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.alertservice;
+package com.aggregator.alert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- alert-service/src/test/java/com/aggregator/alert/AlertServiceApplicationTests.java
+++ alert-service/src/test/java/com/aggregator/alert/AlertServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.alertservice;
+package com.aggregator.alert;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
