d0c0e58f1fa1d8d7449fd73a33c6edacd2c9ebc7
wsujczynski <wojciechsuj@gmail.com>
2026-04-23 11:48:39 +0200

    Implement alert repo with method to find active alerts for given currency code

--- alert-service/src/main/java/com/aggregator/alert/repository/AlertRepository.java
+++ alert-service/src/main/java/com/aggregator/alert/repository/AlertRepository.java
@@ -1,4 +1,12 @@
 package com.aggregator.alert.repository;
 
-public class AlertRepository {
+import com.aggregator.alert.entity.Alert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository()
+public interface AlertRepository extends JpaRepository<Alert, Long> {
+    List<Alert> findAlertByCurrencyCodeAndActiveIsTrue(String currencyCode);
 }
