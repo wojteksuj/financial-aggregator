30b13266f3b773b67fa0abfa6f1f0b6a31afb4d5
wsujczynski <wojciechsuj@gmail.com>
2026-04-23 16:10:30 +0200

    Configurate rabbit

---
+++ alert-service/src/main/java/com/aggregator/alert/config/RabbitConfig.java
@@ -0,0 +1,52 @@
+package com.aggregator.alert.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public static final String MARKET_EXCHANGE = "market.exchange";
+    public static final String ALERT_EXCHANGE = "alert.exchange";
+    public static final String RATES_UPDATED_QUEUE = "rates.updated";
+    public static final String ALERT_TRIGGERED_QUEUE = "alert.triggered";
+
+    @Bean
+    public DirectExchange marketExchange() {
+        return new DirectExchange(MARKET_EXCHANGE);
+    }
+
+    @Bean
+    public DirectExchange alertExchange() {
+        return new DirectExchange(ALERT_EXCHANGE);
+    }
+
+    @Bean
+    public Queue ratesUpdatedQueue() {
+        return new Queue(RATES_UPDATED_QUEUE, true);
+    }
+
+    @Bean
+    public Queue alertTriggeredQueue() {
+        return new Queue(ALERT_TRIGGERED_QUEUE, true);
+    }
+
+    @Bean
+    public Binding ratesUpdatedBinding(Queue ratesUpdatedQueue, DirectExchange marketExchange) {
+        return BindingBuilder.bind(ratesUpdatedQueue)
+                .to(marketExchange)
+                .with(RATES_UPDATED_QUEUE);
+    }
+
+    @Bean
+    public Binding alertTriggeredBinding(Queue alertTriggeredQueue, DirectExchange alertExchange) {
+        return BindingBuilder.bind(alertTriggeredQueue)
+                .to(alertExchange)
+                .with(ALERT_TRIGGERED_QUEUE);
+    }
+}
+
