a8946e5a5d08efc9fa3280564c18ba8247d951cf
wsujczynski <wojciechsuj@gmail.com>
2026-04-25 17:03:03 +0200

    Add endpoint for getting alerts by currency code

--- alert-service/src/main/java/com/aggregator/alert/controller/AlertController.java
+++ alert-service/src/main/java/com/aggregator/alert/controller/AlertController.java
@@ -2,12 +2,14 @@ package com.aggregator.alert.controller;
 
 import com.aggregator.alert.dto.request.CreateAlertRequest;
 import com.aggregator.alert.dto.response.CreateAlertResponse;
+import com.aggregator.alert.dto.response.GetAlertResponse;
 import com.aggregator.alert.service.AlertService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping(("/api/v1/alerts"))
@@ -15,6 +17,11 @@ import org.springframework.web.bind.annotation.*;
 public class AlertController {
 
     private final AlertService alertService;
+
+    @GetMapping
+    public ResponseEntity<List<GetAlertResponse>> getAlertsByCurrencyCode(@RequestParam String currencyCode) {
+        return ResponseEntity.ok(alertService.getAlertsByCurrencyCode(currencyCode));
+    }
 
     @PostMapping
     public ResponseEntity<CreateAlertResponse> createAlert(@Valid @RequestBody CreateAlertRequest createAlertRequest){
