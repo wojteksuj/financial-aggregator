9c3b9b883bb9d2d0c7fe9f970efc0e3485cc7618
wsujczynski <wojciechsuj@gmail.com>
2026-04-26 11:21:22 +0200

    Add rabbit config class

---
+++ market-service/src/main/java/com/aggregator/market/config/RabbitConfig.java
@@ -0,0 +1,32 @@
+package com.aggregator.market.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public static final String MARKET_EXCHANGE = "market.exchange";
+    public static final String RATES_UPDATED_QUEUE = "rates.updated";
+
+    @Bean
+    public DirectExchange marketExchange() {
+        return new DirectExchange(MARKET_EXCHANGE);
+    }
+
+    @Bean
+    public Queue ratesUpdatedQueue() {
+        return new Queue(RATES_UPDATED_QUEUE, true);
+    }
+
+    @Bean
+    public Binding ratesUpdatedBinding(Queue ratesUpdatedQueue, DirectExchange marketExchange) {
+        return BindingBuilder.bind(ratesUpdatedQueue)
+                .to(marketExchange)
+                .with(RATES_UPDATED_QUEUE);
+    }
+}
