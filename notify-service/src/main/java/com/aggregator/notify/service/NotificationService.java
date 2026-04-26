398fa03b4360e8586330cfb6049fe9691bc0ed69
wsujczynski <wojciechsuj@gmail.com>
2026-04-26 21:09:38 +0200

    Add notification service with method processEmail to send and save new notifications

--- notify-service/src/main/java/com/aggregator/notify/service/NotificationService.java
+++ notify-service/src/main/java/com/aggregator/notify/service/NotificationService.java
@@ -1,5 +1,7 @@
 package com.aggregator.notify.service;
 
+import com.aggregator.notify.dto.TriggeredAlertEventDto;
+import com.aggregator.notify.entity.Notification;
 import com.aggregator.notify.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,7 +11,21 @@ import org.springframework.stereotype.Service;
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
+    private final EmailService emailService;
 
+    public void processAlert(TriggeredAlertEventDto eventDto){
+        emailService.sendAlertEmail(eventDto);
 
+        Notification newNotification = new Notification(
+                eventDto.alertId(),
+                eventDto.currencyCode(),
+                eventDto.thresholdRate(),
+                eventDto.currentRate(),
+                eventDto.higher(),
+                "EMAIL",
+                eventDto.triggeredAt()
+        );
+        notificationRepository.save(newNotification);
+    }
 
 }
