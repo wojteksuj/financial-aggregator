b822e64e1314ef4529e526f294449b84246356d6
wsujczynski <wojciechsuj@gmail.com>
2026-04-26 13:18:19 +0200

    Initiate microservice notify-service

---
+++ notify-service/src/test/java/com/aggregator/notifyservice/NotifyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggregator.notifyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
