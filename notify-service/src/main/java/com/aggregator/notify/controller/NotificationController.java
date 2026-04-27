4d9c4d921cf4402aab1c6a43cab86ed781ec9a7a
wsujczynski <wojciechsuj@gmail.com>
2026-04-27 12:22:27 +0200

    Add service to get list of notifications

--- notify-service/src/main/java/com/aggregator/notify/controller/NotificationController.java
+++ notify-service/src/main/java/com/aggregator/notify/controller/NotificationController.java
@@ -1,8 +1,7 @@
 package com.aggregator.notify.controller;
 
 import com.aggregator.notify.dto.response.GetNotificationResponse;
-import com.aggregator.notify.entity.Notification;
-import com.aggregator.notify.repository.NotificationRepository;
+import com.aggregator.notify.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,28 +15,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NotificationController {
 
-    private final NotificationRepository notificationRepository;
+    private final NotificationService notificationService;
 
     @GetMapping
     public List<GetNotificationResponse> getNotifications(
             @RequestParam(required = false) String currency) {
-
-        List<Notification> notifications = (currency != null)
-                ? notificationRepository.findByCurrencyCode(currency)
-                : notificationRepository.findAll();
-
-        return notifications.stream()
-                .map(n -> new GetNotificationResponse(
-                        n.getId(),
-                        n.getAlertId(),
-                        n.getCurrencyCode(),
-                        n.getThresholdRate(),
-                        n.getCurrentRate(),
-                        n.getDifference(),
-                        n.isHigher(),
-                        n.getType(),
-                        n.getTriggeredAt(),
-                        n.getSentAt()
-                )).toList();
+        return notificationService.getNotifications(currency);
     }
 }
