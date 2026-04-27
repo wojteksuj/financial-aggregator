cda31374fe62d554ba4fd41bceba0ed5002e6167
wsujczynski <wojciechsuj@gmail.com>
2026-04-27 12:22:44 +0200

    Add method to get list of notifications by currency

--- notify-service/src/main/java/com/aggregator/notify/service/NotificationService.java
+++ notify-service/src/main/java/com/aggregator/notify/service/NotificationService.java
@@ -1,10 +1,13 @@
 package com.aggregator.notify.service;
 
 import com.aggregator.notify.dto.TriggeredAlertEventDto;
+import com.aggregator.notify.dto.response.GetNotificationResponse;
 import com.aggregator.notify.entity.Notification;
 import com.aggregator.notify.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,7 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final EmailService emailService;
 
-    public void processAlert(TriggeredAlertEventDto eventDto){
+    public void processAlert(TriggeredAlertEventDto eventDto) {
         emailService.sendAlertEmail(eventDto);
 
         Notification newNotification = new Notification(
@@ -28,4 +31,23 @@ public class NotificationService {
         notificationRepository.save(newNotification);
     }
 
+    public List<GetNotificationResponse> getNotifications(String currency) {
+        List<Notification> notifications = (currency != null)
+                ? notificationRepository.findByCurrencyCode(currency)
+                : notificationRepository.findAll();
+
+        return notifications.stream()
+                .map(n -> new GetNotificationResponse(
+                        n.getId(),
+                        n.getAlertId(),
+                        n.getCurrencyCode(),
+                        n.getThresholdRate(),
+                        n.getCurrentRate(),
+                        n.getDifference(),
+                        n.isHigher(),
+                        n.getType(),
+                        n.getTriggeredAt(),
+                        n.getSentAt()
+                )).toList();
+    }
 }
